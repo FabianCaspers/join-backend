34c280eebe039af93229e401612f2f35bf7a9905
FabianCaspers <fabian.caspers1308@gmail.com>
2023-08-17 19:17:39 +0200

    finish contacts

--- join_backend/contacts/models.py
+++ join_backend/contacts/models.py
@@ -6,6 +6,7 @@ class Contacts(models.Model):
     email = models.EmailField(unique=True)
     number = models.CharField(max_length=20)
     company = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} {self.email}"
