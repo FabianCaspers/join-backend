78a9c361953d58b3029385ed7bd324979f579d07
FabianCaspers <fabian.caspers1308@gmail.com>
2023-08-23 17:29:35 +0200

    reset-password

--- join_backend/join_backend/urls.py
+++ join_backend/join_backend/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from joinlogin import views
-from joinlogin.views import LoginView, RegisterView, current_user_view
+from joinlogin.views import LoginView, RegisterView, current_user_view, send_reset_email, reset_password_confirm
 from addTask.models import AddTask
 from rest_framework.routers import DefaultRouter
 from addTask.views import AddTaskViewSet
@@ -19,5 +19,7 @@ urlpatterns = [
     path('current_user/', current_user_view, name='current-user'),
     path('', include(router.urls)),
     path('delete_all_tasks/', addTask_views.delete_all_tasks, name='delete-all-tasks'),
-    path('contacts/', include('contacts.urls'))
+    path('contacts/', include('contacts.urls')),
+    path('send_reset_email/', views.send_reset_email, name='send_reset_email'),
+    path('reset_password/<uidb64>/<token>/', views.reset_password_confirm, name='password_reset_confirm'),
 ]
--- join_backend/joinlogin/views.py
+++ join_backend/joinlogin/views.py
@@ -12,6 +12,14 @@ from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from .models import PasswordReset
+from django.utils.encoding import force_str
+
+
 
 # Create your views here.
 class LoginView(APIView):
@@ -57,3 +65,46 @@ class RegisterView(APIView):
 def current_user_view(request):
     serializer = CurrentUserSerializer(request.user)
     return Response(serializer.data)
+
+
+
+
+
+@api_view(['POST'])
+def send_reset_email(request):
+    email = request.data.get('email')
+    user = User.objects.get(email=email)
+    
+    # Generate Token
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    
+    # Safe Token to Backend
+    PasswordReset.objects.create(user=user, token=token)
+    
+    # Send Mail
+    mail_subject = 'Reset your password.'
+    message = f"Here is your link to reset your password: https://darkjoin.fabiancaspers.com/html/reset-password.html/{uid}/{token}/"
+    send_mail(mail_subject, message, 'fabian.caspers1308@gmail.com', [email], fail_silently=False)
+    
+    return Response({"message": "Email sent."})
+
+
+@api_view(['POST'])
+def reset_password_confirm(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except User.DoesNotExist:
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        # Token is awiabled, set password
+        new_password = request.data.get('password')
+        user.set_password(new_password)
+        user.save()
+        # After use Token, delete Token
+        PasswordReset.objects.filter(user=user, token=token).delete()
+        return Response({"message": "Password reset successful."})
+    else:
+        return Response({"error": "Invalid token."}, status=400)
