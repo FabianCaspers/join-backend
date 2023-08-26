aa35fd9e7ab9015788e867671dad6e7f9fa520f5
FabianCaspers <fabian.caspers1308@gmail.com>
2023-08-26 19:54:58 +0200

    add permissions

--- join_backend/tasks/views.py
+++ join_backend/tasks/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -14,7 +14,10 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 @api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def delete_all_tasks(request):
     tasks = Task.objects.all()
     tasks.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
