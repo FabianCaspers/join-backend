fbef9ee3ceb2ed2e607235f56d6a0af8d7a4a994
FabianCaspers <fabian.caspers1308@gmail.com>
2023-08-26 18:59:39 +0200

    add permissions

--- join_backend/contacts/views.py
+++ join_backend/contacts/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Contacts
 from .serializers import ContactsSerializer
 
 class ContactsViewSet(viewsets.ModelViewSet):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- join_backend/tasks/views.py
+++ join_backend/tasks/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.response import Response
@@ -8,6 +8,7 @@ from rest_framework import status
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 @api_view(['DELETE'])
 def delete_all_tasks(request):
