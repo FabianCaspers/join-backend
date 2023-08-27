38f1eb686661775c185cf6f6dcde8bbf7f3191ec
FabianCaspers <fabian.caspers1308@gmail.com>
2023-08-27 12:47:20 +0200

    add some tests

--- join_backend/contacts/tests.py
+++ join_backend/contacts/tests.py
@@ -1,13 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import Contacts
-
-# Create your tests here.
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 class ContactsModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Contacts.objects.create(name='John', email='john@example.com', number='123456789', company='Bad-Company')
+        cls.user = User.objects.create_user(username='testuser', password='testpass', email='testuser@example.com')
+        cls.token, _ = Token.objects.get_or_create(user=cls.user)
+        cls.contact = Contacts.objects.create(name='John', email='john@example.com', number='123456789', company='Bad-Company')
+    
+    def setUp(self):
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+  
+    def set_auth_credentials(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
 
     def test_name_label(self):
         contact = Contacts.objects.get(id=1)
@@ -18,3 +29,28 @@ class ContactsModelTest(TestCase):
         contact = Contacts.objects.get(id=1)
         field_label = contact._meta.get_field('email').verbose_name
         self.assertEquals(field_label, 'email')
+        
+    def test_post_contact(self):
+        self.set_auth_credentials()
+        data = {
+            'name': 'Jane',
+            'email': 'jane@example.com',
+            'number': '987654321',
+            'company': 'Good-Company',
+        }
+        response = self.client.post('https://fabiancaspersdjango.pythonanywhere.com/contacts/', data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'Jane')
+
+    def test_get_contact(self):
+        self.set_auth_credentials()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get(f'/contacts/{self.contact.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'John')
+
+
+    def test_delete_contact(self):
+        self.set_auth_credentials()
+        response = self.client.delete(f'https://fabiancaspersdjango.pythonanywhere.com/contacts/{self.contact.id}/')
+        self.assertEqual(response.status_code, 204)
--- join_backend/tasks/tests.py
+++ join_backend/tasks/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Task
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 # Create your tests here.
 
@@ -7,7 +10,9 @@ from .models import Task
 class TasksModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Task.objects.create(
+        cls.user = User.objects.create_user(username='testuser', password='testpass', email='testuser@example.com')
+        cls.token, _ = Token.objects.get_or_create(user=cls.user)
+        cls.task = Task.objects.create(
             title="Test Task",
             description="Test Description",
             category="Test Category",
@@ -16,6 +21,10 @@ class TasksModelTest(TestCase):
             prio="High",
             status="todo",
         )
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_title_label(self):
         task = Task.objects.get(id=1)
@@ -26,3 +35,34 @@ class TasksModelTest(TestCase):
         task = Task.objects.get(id=1)
         field_label = task._meta.get_field("description").verbose_name
         self.assertEquals(field_label, "description")
+        
+    def test_get_task(self):
+        response = self.client.get(f'https://fabiancaspersdjango.pythonanywhere.com/task/{self.task.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Test Task')
+
+    def test_post_task(self):
+        data = {
+            'title': 'New Task',
+            'description': 'New Description',
+            'category': 'New Category',
+            'assigned': 'New Assignee',
+            'dueDate': '2023-08-12',
+            'prio': 'Low',
+            'status': 'todo',
+        }
+        response = self.client.post('https://fabiancaspersdjango.pythonanywhere.com/task/', data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['title'], 'New Task')
+
+    def test_patch_task(self):
+        data = {
+            'title': 'Updated Task',
+        }
+        response = self.client.patch(f'https://fabiancaspersdjango.pythonanywhere.com/task/{self.task.id}/', data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Updated Task')
+
+    def test_delete_task(self):
+        response = self.client.delete(f'https://fabiancaspersdjango.pythonanywhere.com/task/{self.task.id}/')
+        self.assertEqual(response.status_code, 204)
